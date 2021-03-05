83077a473d7c0cbb3cdf97d6a0e8d127fd3da50a
yozora <2540117053@qq.com>
2021-03-05 15:42:55 +0800

    修改API地址

--- Ethereum&Conflux/src/apis/server/api.js
+++ Ethereum&Conflux/src/apis/server/api.js
@@ -1,4 +1,4 @@
 export default {
-  getTrustMe: 'http://182.254.241.232:8081/api/log/getTrustYou',
-  getYouTrust: 'http://182.254.241.232:8081/api/log/getYouTrust'
+  getTrustMe: 'https://cfxtrust.loopss.me/api/log/getTrustYou',
+  getYouTrust: 'https://cfxtrust.loopss.me/api/log/getYouTrust'
 }
